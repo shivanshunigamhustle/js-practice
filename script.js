4cb28bb5f066181d5429e0cfd003fbe0e7d72d15
shivanshunigam <shivanshunigam6@gmail.com>
2025-11-01 19:00:41 +0530

    shyam

---
+++ script.js
@@ -0,0 +1,26 @@
+// for(let i=1;i<=10;i++){
+//     console.log(i);
+    
+// }
+// let m=0;
+// for(let i=1;i<=12;i++){
+//     if(i!==2 && i!==3){
+//        m=i;
+//        console.log(m);
+        
+
+//     }
+    
+    
+// }
+for(let i=10;i>0;i--){
+    console.log(i);
+    
+}
+for(let i=1;i<10;i++){
+   
+    if(i%2===0)console.log(`${i}-Even`);
+    else console.log(`${i}-odd`);
+    
+    
+}
