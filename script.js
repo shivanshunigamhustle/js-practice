975286df84f8d31c6a24daf6aeeea543c6c841bc
shivanshunigam <shivanshunigam6@gmail.com>
2025-11-02 17:56:57 +0530

    hum

--- script.js
+++ script.js
@@ -13,14 +13,41 @@
     
     
 // }
-for(let i=10;i>0;i--){
-    console.log(i);
+// for(let i=10;i>0;i--){
+//     console.log(i);
     
-}
-for(let i=1;i<10;i++){
+// }
+// for(let i=1;i<10;i++){
    
-    if(i%2===0)console.log(`${i}-Even`);
-    else console.log(`${i}-odd`);
+//     if(i%2===0)console.log(`${i}-Even`);
+//     else console.log(`${i}-odd`);
     
     
+//}
+// let num=+prompt("enter the age")
+// if(num>=18){
+//     console.log("eligible");
+    
+// }else{
+//     console.log("not eligible");
+    
+// }
+let age = prompt("enter the age");
+
+ if(age===null){
+    console.error("you entered cancel value");
+    
+}else{
+    if(age.trim()===""){
+        console.error("dhang se likh");
+    }else{
+        age=Number(age.trim());
+        if(isNaN(age)){
+            console.error("number de de bha")
+        }else{
+            console.log("perfect number h");
+            
+        }
+    }
 }
+
